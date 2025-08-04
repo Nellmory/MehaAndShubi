569f0a3f3ce901411b0e576a6dc229e7ea706866
agapo <agapova.nelli@gmail.com>
2025-08-04 18:56:36 +0300

    from sqlite to postgre

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -74,10 +74,16 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mehaandshubi_db',
+        'USER': 'postgres',
+        'PASSWORD': '1234567',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
